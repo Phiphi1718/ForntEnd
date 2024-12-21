3aa2414c0a1579b4c9c356a61087c89bbc969fe1
Phiphi1718 <tangocphiphi1710@gmail.com>
2024-12-21 13:22:40 +0700

    update banner

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -131,15 +131,15 @@ const ProductDetail = ({ onClose }) => {
 
   if (!product) {
     return (
-      <div className="cube-loader"> Loading...
-        <div className="cube-top"></div>
-        <div className="cube-wrapper">
-          <span style={{ "--i": 0 }} className="cube-span"></span>
-          <span style={{ "--i": 1 }} className="cube-span"></span>
-          <span style={{ "--i": 2 }} className="cube-span"></span>
-          <span style={{ "--i": 3 }} className="cube-span"></span>
+<div id="page">
+        <div id="container">
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="h3">No Products</div>
         </div>
-      </div>
+</div>
     );
   }
   
